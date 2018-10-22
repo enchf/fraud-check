79a9b60a5f5d05fc1c5fa4665c4f4479f76bfd68
enchf <e.ernesto.espinosa@gmail.com>
2018-10-22 07:41:55 -0500

    [EX3] Autowire the consolidator lists

--- src/main/java/com/frod/fraudcheck/score/Consolidator.java
+++ src/main/java/com/frod/fraudcheck/score/Consolidator.java
@@ -1,10 +1,14 @@
 package com.frod.fraudcheck.score;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Consolidator<T> implements Scorer<T> {
 
-    private List<Scorer<T>> scorers;
+    @Autowired(required = false)
+    private List<Scorer<T>> scorers = new ArrayList<>();
     private static final Double WITH_NO_SCORERS = 0.0;
 
     @Override
